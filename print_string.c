ac494b8fbb97e3a192ca74c0e4a038c808dbcf59
cazicruz <dcazicruz@icloud.com>
2022-08-08 04:44:44 -0700

    dave adding

---
+++ print_string.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+int print_string(char *s)
+{
+	int i;
+	hello
+	for (i == 0; s[i] != '\0'; i++)
+	{
+		_putchar(s[i]);
+	}
+	return(i - 1);
+}
