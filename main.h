4adedb4dcef71ef1dd8552c53ac20e83b949de01
Progress ochuko Eyaadah <koxygraphicsgmail.com>
2022-08-08 19:02:11 -0700

    done

--- main.h
+++ main.h
@@ -1,8 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
 int _putchar(char c);
 int _printf(const char *format, ...);
 int print_string(char *s);
-
+int print_buf(char *buf, unsigned int nbuf);
+int print_int(va_list arguments, char *buf, unsigned int ibuf);
 #endif
