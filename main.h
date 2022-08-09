8fde94cd5cdb940b43deb3d84824098cf3c4e964
Progress ochuko Eyaadah <koxygraphicsgmail.com>
2022-08-08 18:41:27 -0700

    done

--- main.h
+++ main.h
@@ -4,5 +4,7 @@
 int _putchar(char c);
 int _printf(const char *format, ...);
 int print_string(char *s);
+int print_buf(char *buf, unsigned int nbuf);
+int print_int(va_list arguments, char *buf, unsigned int ibuf);
 
 #endif
